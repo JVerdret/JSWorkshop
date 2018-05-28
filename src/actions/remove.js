a41935a788385fadcfdd4ec00dc3633024dd4665
Kenpatsu <kenpatsu@hotmail.fr>
2018-05-28 21:54:13 +0200

    Other remove method

--- src/actions/remove.js
+++ src/actions/remove.js
@@ -14,7 +14,17 @@ import { setState, getState } from "../store";
 const remove = (i) => {
     const tablo = getState();
     var suppr = tablo.indexOf(String(i));
-    tablo.splice(suppr, suppr + 1);
+    delete tablo[suppr];
+    /*for (var j = 0; j < tablo.length ; j++) {
+        console.log(tablo[j]);
+        if (String(tablo[j]) == "undefined") {
+            for (var k = j ; k < tablo.length - 1 ;k ++) {
+                tablo[k] = tablo[k + 1];
+            }
+            remove(tablo.length - 1);
+        }
+    }*/
+    //tablo.splice(suppr, suppr + 1);
     setState(tablo);
 };
 
